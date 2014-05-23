f0ea0deb6995c8347ea0f7dcc4d7960124f005f6
Gleyser Bonfim Guimaraes 21121079 <gleyserbg@lcc2-12.lcc.ufcg.edu.br>
2014-05-23 17:03:49 -0300

    Mudando a página dinamica

--- dinamica/javascript/meuscript.js
+++ dinamica/javascript/meuscript.js
@@ -1,12 +1,12 @@
 ﻿function adicionaTema() {
-	$nome = $('#temasAprender').val();
+	$nome = $('#nome').val();
  $id = $nome.split(" ").join("");  // tira os espaços em branco do nome
 
 	if ($('#' + $id).length) { // se já existir uma div com esse id lança uma msg de erro
 		alert("Tema já existe!");
 	} else {
 
-		$('#series').append('<div class="label label-danger col-sm-5"  id="' + $id + '"> </div>');
+		$('#temas').append('<div class="label label-danger col-sm-5"  id="' + $id + '"> </div>');
 		$divSeriado = $('#' + $id);
 		$divSeriado.append("<h3>" + $nome);
 		$divSeriado.append("<br /> ");
@@ -19,12 +19,5 @@
 
 function limpaForms() {
 	$('#nome').val('');
-	$('#descricao').val('');
-	$('#lancamento').val('');
+	
 }
-
-function removerSerie(){
-	$nome = $('#nomeRem').val();
-	$id = $nome.split(" ").join("");  // tira os espaços em branco do nome
-	$('#'+ $id).fadeOut();	
-} 
