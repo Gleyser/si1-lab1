8e514ba92bf28cb4214c25526316c92725df2959
Gleyser Guimaraes <gleyser.bonfim.guimaraes@ccc.ufcg.edu.br>
2014-05-23 13:14:16 -0300

    Adicionando as pastas do lab1

---
+++ dinamica/javascript/exemplo.js
@@ -0,0 +1,33 @@
+﻿function adicionaSerie() {
+	$nome = $('#nome').val();
+ $id = $nome.split(" ").join("");  // tira os espaços em branco do nome
+
+	if ($('#' + $id).length) { // se já existir uma div com esse id lança uma msg de erro
+		alert("Seriado já existe!");
+	} else {
+
+		$('#series').append('<div class="label label-danger col-sm-5"  id="' + $id + '"> </div>');
+		$divSeriado = $('#' + $id);
+		$divSeriado.append("<h3>" + $nome);
+		$divSeriado.append("<br /> ");
+		$divSeriado.append("<strong> Descrição: </strong>" + $descricao);
+		$divSeriado.append("<br /> ");
+		$divSeriado.append("<strong> Lançamento: </strong>" + $lancamento);
+		$divSeriado.append("<br /> ");
+
+		limpaForms();
+		$divSeriado.hide().fadeIn();
+	}
+}
+
+function limpaForms() {
+	$('#nome').val('');
+	$('#descricao').val('');
+	$('#lancamento').val('');
+}
+
+function removerSerie(){
+	$nome = $('#nomeRem').val();
+	$id = $nome.split(" ").join("");  // tira os espaços em branco do nome
+	$('#'+ $id).fadeOut();	
+} 
