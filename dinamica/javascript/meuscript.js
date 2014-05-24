f4212694915964fd504b124c5ea8613ae427e7ad
Gleyser Guimaraes <gleyser.bonfim.guimaraes@ccc.ufcg.edu.br>
2014-05-24 02:06:32 -0300

    Mudando script

--- dinamica/javascript/meuscript.js
+++ dinamica/javascript/meuscript.js
@@ -1,23 +1,8 @@
-﻿function adicionaTema() {
-	$nome = $('#nome').val();
- $id = $nome.split(" ").join("");  // tira os espaços em branco do nome
-
-	if ($('#' + $id).length) { // se já existir uma div com esse id lança uma msg de erro
-		alert("Tema já existe!");
-	} else {
-
-		$('#temas').append('<div class="label label-danger col-sm-5"  id="' + $id + '"> </div>');
-		$divSeriado = $('#' + $id);
-		$divSeriado.append("<h3>" + $nome);
-		$divSeriado.append("<br /> ");
-		
-
-		limpaForms();
-		$divSeriado.hide().fadeIn();
-	}
-}
-
-function limpaForms() {
-	$('#nome').val('');
-	
+﻿function inserir()
+{
+var x = document.getElementById("Aprender");
+var option = document.createElement("option");
+option.text = $('#nome').val();
+x.add(option);
+$('#nome').val('');
 }
